24235d281e2cea238df5732baf5fbaeec8460a51
sudheerdagar <sandstormarriving101@gmail.com>
2024-05-13 15:14:03 +0530

    initial setup

---
+++ gateway/index.js
@@ -0,0 +1,20 @@
+const express=require('express')
+const cors=require('cors')
+const proxy=require('express-http-proxy')
+
+const app=express();
+
+app.use(cors());
+app.use(express.json());
+
+
+
+app.use('/customer',proxy('http://localhost:8001'));
+
+app.use('/',proxy('http://localhost:8002'));//root as well as /products
+app.use('/shopping',proxy('http://localhost:8003'));
+
+
+app.listen(8000,()=>{
+    console.log( "Gateway is running on port 8000")
+})
