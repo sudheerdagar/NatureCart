188c3674f0f840283cf335fc3d83759efa0ad612
sudheerdagar <sandstormarriving101@gmail.com>
2024-05-22 17:56:18 +0530

    integrated message broker

--- gateway/index.js
+++ gateway/index.js
@@ -1,20 +1,16 @@
-const express=require('express')
-const cors=require('cors')
-const proxy=require('express-http-proxy')
+const express = require("express");
+const cors = require("cors");
+const proxy = require("express-http-proxy");
 
-const app=express();
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use("/customer", proxy("http://localhost:8001"));
+app.use("/shopping", proxy("http://localhost:8003"));
+app.use("/", proxy("http://localhost:8002")); // products
 
-
-app.use('/customer',proxy('http://localhost:8001'));
-
-app.use('/',proxy('http://localhost:8002'));//root as well as /products
-app.use('/shopping',proxy('http://localhost:8003'));
-
-
-app.listen(8000,()=>{
-    console.log( "Gateway is running on port 8000")
-})
+app.listen(8000, () => {
+  console.log("Gateway is Listening to Port 8000");
+});
